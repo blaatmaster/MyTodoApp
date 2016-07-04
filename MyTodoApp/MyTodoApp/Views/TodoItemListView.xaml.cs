ee7bdd8534fe1997dc322b881ba17cd7365faf4d
Onzinnigezooi <no@mail.com>
2016-07-04 21:07:14 +0200

    Added Basic Empty TodoItem classes

---
+++ MyTodoApp/MyTodoApp/Views/TodoItemListView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MyTodoApp.Views
+{
+    public partial class TodoItemListView : ContentPage
+    {
+        public TodoItemListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
